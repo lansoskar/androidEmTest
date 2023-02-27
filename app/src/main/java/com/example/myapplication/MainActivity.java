d81f0349c6b94e8e0fbfbd50399b8d5be6477a54
Oskar <lansoskar@gmail.com>
2023-02-27 10:42:58 +0100

    ini

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,23 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+String TAG = this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity", "Activity Created");
+        Log.d(TAG, "Activity Created");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("MainActivity", "Activity paused");
+        Log.d(TAG, "Activity paused");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("MainActivity", "Activity resuming");
+        Log.d(TAG, "Activity resuming");
     }
 }
